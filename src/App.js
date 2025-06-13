4be7e2de5fe3e3852cd8e793c5b28cdc61f9d33f
Shivam kumar <sk366035@gmail.com>
2025-06-13 14:58:00 +0530

    This is start of React

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Phone</li>
+      <li>House</li>
+      <li>Temp</li>
+    </nav>
+
+    <div className="container">
+      <h1>Hello</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere sapiente corporis voluptatibus minus quod esse incidunt eveniet architecto accusantium optio.</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
