b97654c894974f84c8d8cebedb94ec2bfa6a465b
Shivam kumar <sk366035@gmail.com>
2025-06-16 11:42:01 +0530

    Props and PropTypes in React

---
+++ module1.js
@@ -0,0 +1,5 @@
+import dza, {a,c,d} from './module2.js'
+console.log(dza);
+console.log(c);
+console.log(d);
+console.log(a);
---
+++ module2.js
@@ -0,0 +1,9 @@
+const a="Shivam";
+const b="Singh";
+const c="Rina devi";
+const d="Dhirendra pd singh";
+
+export default d;
+export {a};
+export {c};
+export {d};
